1ff0d9bdb972e1f2223fb1f364e3d8c38b53fce2
Jesús Enrique Rascón <blograso@gmail.com>
2022-11-05 12:58:23 +0100

    a ver si va esta mierda

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -3,14 +3,10 @@
 import fs from "fs";
 
 export function load() {
-  let data = fs.readFileSync(
-    "./static/focal-es.tsv",
-    "utf-8",
-    (error, data) => {
-      if (error) throw error;
-      return data;
-    }
-  );
+  let data = fs.readFileSync("./focal-es.tsv", "utf-8", (error, data) => {
+    if (error) throw error;
+    return data;
+  });
 
   return { data };
 }
