fc74d9c5a414d1a479a1d7bab19d4b8334c93372
Jesús Rascón <blograso@gmail.com>
2022-11-08 21:05:44 +0100

    update about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,9 +1,20 @@
-<div class="flex flex-col space-y-10 mx-auto max-w-sm md:max-w-md">
-  <div class="text-3xl font-semibold font-title">¿Qué es esto?</div>
-  <div class="">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci in
-    explicabo exercitationem molestiae necessitatibus, enim soluta voluptas sit?
-    Ea earum cupiditate saepe natus reiciendis adipisci similique fugit
-    perspiciatis, quaerat dicta!
+<div class="flex flex-col space-y-10 mx-auto">
+  <div class="text-3xl font-semibold font-title text-main">
+    El proyecto FOCAL-ES
+  </div>
+  <div class="text-justify space-y-3">
+    <p>
+      FOCAL-ES es una base de datos de más de 1.300 ejemplos de uso de
+      diferentes partículas focales. El desarrollo de este estudio ha sido
+      llevado a cabo por los investigadores y profesores de la Universidad de
+      Jaén <b>Ventura Salazar, Elena Felíu</b> y <b>María del Carmen Conti</b>.
+    </p>
+
+    <p>
+      Este enorme proyecto de investigación lleva en proceso más de 10 años, en
+      los que los ejemplos han sido extraídos, curados y analizados, con objeto
+      de ofrecer una perspectiva clara y concisa de su uso y relevancia en el
+      uso del lenguaje.
+    </p>
   </div>
 </div>
