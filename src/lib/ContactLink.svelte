0852bc971e983e391d20d6a397ccf42c1e1f4ece
Jesús Rascón <blograso@gmail.com>
2022-11-25 22:48:19 +0100

    contactlink improvement

--- src/lib/ContactLink.svelte
+++ src/lib/ContactLink.svelte
@@ -7,7 +7,10 @@
   export let link;
 </script>
 
-<a href={link}>
+<a
+  class="hover:bg-accent hover:bg-opacity-30 px-2 py-2 w-max rounded-md transition-colors"
+  href={link}
+>
   <div class="font-xl text-main font-bold flex flex-row items-center">
     <span>
       <Link class="mr-3" weight="duotone" />
@@ -15,5 +18,7 @@
 
     {title}
   </div>
-  <div class="text-accent-darker">{link}</div>
+  <div class="text-main text-opacity-60 font-mono text-sm">
+    {link}
+  </div>
 </a>
